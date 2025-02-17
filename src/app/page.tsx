3990663b74913d88c577951c75c1b93ee9c61106
ThatDevShparki <shparki@thatdev.io>
2025-02-17 14:30:05 -0600

    fix: remove posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { api } from "@/trpc/server";
-
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div>The Price of Eggs is TOO DAMN HIGH!</div>
