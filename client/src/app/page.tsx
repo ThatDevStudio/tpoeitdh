2be5f65d69a90859e2d265d78db62a07c4a217c1
ThatDevShparki <shparki@thatdev.io>
2025-02-22 11:24:40 -0600

    feat: adding service

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-start">
+      <div className="p-4 font-sans text-xl font-bold uppercase">
+        The Price of Eggs is TOO DAMN HIGH
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div>The Price of Eggs is TOO DAMN HIGH!</div>
-    </div>
-  );
-}
