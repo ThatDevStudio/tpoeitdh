74aac05dca2c9f1ca38fd24a0c23a83c2f1af490
ThatDevShparki <shparki@thatdev.io>
2025-03-04 08:04:52 -0600

    feat: update fec api

--- src/server/services/fec/api/candidates.ts
+++ src/server/services/fec/api/candidates.ts
@@ -1,36 +1,44 @@
-import { env } from "@/env";
+import { cachedFecFetch } from "@/server/services/fec/api";
 import { type Candidate } from "@/server/services/fec/types/candidate";
 import { type PaginatedResponse } from "@/server/services/fec/types/pagination";
 
 export const fetchCandidates = async (): Promise<
   PaginatedResponse<Candidate>
 > => {
-  try {
-    const url = new URL(
-      `${env.FEC_API_HOST}/${env.FEC_API_VERSION}/candidates`,
-    );
-    url.search = new URLSearchParams([
-      ["is_active_candidate", "true"],
-      ["candidate_status", "C"],
-      ["per_page", "100"],
-      ["incumbent_challenge", "I"],
-      ["office", "H"],
-      ["office", "S"],
-    ]).toString();
+  return cachedFecFetch("candidates", {
+    is_active_candidate: "true",
+    candidate_status: "C",
+    per_page: "100",
+    incumbent_challenge: "I",
+    office: ["H", "S"],
+  });
 
-    const response = await fetch(url, {
-      headers: {
-        accept: "application/json",
-        "x-api-key": env.GOV_API_KEY,
-      },
-    });
-    if (!response.ok) {
-      console.log(await response.text(), url);
-      throw new Error("Failed to fetch candidates");
-    }
-    return (await response.json()) as PaginatedResponse<Candidate>;
-  } catch (error) {
-    console.error("Error fetching candidates", error);
-    throw error;
-  }
+  // try {
+  //   const url = new URL(
+  //     `${env.FEC_API_HOST}/${env.FEC_API_VERSION}/candidates`,
+  //   );
+  //   url.search = new URLSearchParams([
+  //     ["is_active_candidate", "true"],
+  //     ["candidate_status", "C"],
+  //     ["per_page", "100"],
+  //     ["incumbent_challenge", "I"],
+  //     ["office", "H"],
+  //     ["office", "S"],
+  //   ]).toString();
+
+  //   const response = await fetch(url, {
+  //     headers: {
+  //       accept: "application/json",
+  //       "x-api-key": env.GOV_API_KEY,
+  //     },
+  //   });
+  //   if (!response.ok) {
+  //     console.log(await response.text(), url);
+  //     throw new Error("Failed to fetch candidates");
+  //   }
+  //   return (await response.json()) as PaginatedResponse<Candidate>;
+  // } catch (error) {
+  //   console.error("Error fetching candidates", error);
+  //   throw error;
+  // }
 };
---
+++ src/server/services/fec/api/index.ts
@@ -0,0 +1,18 @@
+import { env } from "@/env";
+import { cachedFetch } from "@/server/services";
+
+export const cachedFecFetch = async <T>(
+  path: string,
+  params: Record<string, string | string[]>,
+): Promise<T> => {
+  return cachedFetch<T>(
+    env.FEC_API_HOST,
+    env.FEC_API_VERSION,
+    {
+      accept: "application/json",
+      "x-api-key": env.GOV_API_KEY,
+    },
+    path,
+    params,
+  );
+};
