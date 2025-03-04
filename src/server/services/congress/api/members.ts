0b4f1625d2b3bc9919a4ff75a9b1bf7a9c0a2719
ThatDevShparki <shparki@thatdev.io>
2025-03-04 12:56:33 -0600

    feat: members pagination

--- src/server/services/congress/api/members.ts
+++ src/server/services/congress/api/members.ts
@@ -4,21 +4,19 @@ import { type Member } from "@/server/services/congress/types/member";
 export const fetchMembers = async () => {
   const members: Member[] = [];
 
-  const response = await cachedCongressFetch<{ members: Member[] }>("member", {
-    format: "json",
-    limit: "250",
-    currentMember: "true",
-  });
-  members.push(...response.members);
+  while (true) {
+    const response = await cachedCongressFetch<{ members: Member[] }>(
+      "member",
+      {
+        format: "json",
+        offset: members.length.toString(),
+        limit: "250",
+        currentMember: "true",
+      },
+    );
+    members.push(...response.members);
 
-  // while (response.pagination.next) {
-  //   response = await cachedCongressFetchRaw(response.pagination.next, {
-  //     format: "json",
-  //     limit: "250",
-  //     currentMember: "true",
-  //   });
-  //   members.push(...response.members);
-  // }
-
+    if (response.pagination.count == members.length) break;
+  }
   return members;
 };
