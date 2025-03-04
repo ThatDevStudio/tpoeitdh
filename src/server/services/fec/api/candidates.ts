d8318c1902d465b344f8b69b8bf4c0f838b67992
ThatDevShparki <shparki@thatdev.io>
2025-03-04 08:09:37 -0600

    feat: update candidates

--- src/server/services/fec/api/candidates.ts
+++ src/server/services/fec/api/candidates.ts
@@ -12,33 +12,4 @@ export const fetchCandidates = async (): Promise<
     incumbent_challenge: "I",
     office: ["H", "S"],
   });
-
-  // try {
-  //   const url = new URL(
-  //     `${env.FEC_API_HOST}/${env.FEC_API_VERSION}/candidates`,
-  //   );
-  //   url.search = new URLSearchParams([
-  //     ["is_active_candidate", "true"],
-  //     ["candidate_status", "C"],
-  //     ["per_page", "100"],
-  //     ["incumbent_challenge", "I"],
-  //     ["office", "H"],
-  //     ["office", "S"],
-  //   ]).toString();
-
-  //   const response = await fetch(url, {
-  //     headers: {
-  //       accept: "application/json",
-  //       "x-api-key": env.GOV_API_KEY,
-  //     },
-  //   });
-  //   if (!response.ok) {
-  //     console.log(await response.text(), url);
-  //     throw new Error("Failed to fetch candidates");
-  //   }
-  //   return (await response.json()) as PaginatedResponse<Candidate>;
-  // } catch (error) {
-  //   console.error("Error fetching candidates", error);
-  //   throw error;
-  // }
 };
